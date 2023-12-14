6f60cbacd73bf112347694ab46e350a0f9d844ee
hannahdriver <36014350+hannahdriver@users.noreply.github.com>
2023-12-14 16:07:39 -0500

    Create calculate.sh

---
+++ tests/calculate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd $1
+
+# We have two main output files, .tsv files with allele predictions
+
+echo ".tsv files:"
+find . -name "*hla_genotype.tsv" | xargs md5sum
+find . -name "*kir_genotype.tsv" | xargs md5sum
